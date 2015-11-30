cb325974b2cc6b32d5bc5186acd9031db98673cd
Rafael Polo <rafael@tacticaltech.org>
2015-11-30 17:39:19 +0100

    require jquery for rails base

--- lib/ttc_rails_base/engine.rb
+++ lib/ttc_rails_base/engine.rb
@@ -2,25 +2,19 @@ module TtcRailsBase
   class Engine < ::Rails::Engine
 
     require 'slim'
+    require 'jquery/rails'
 
+    module TtcRailsBase
+      class Application < Rails::Application
 
+       config.generators do |g|
+          g.template_engine :slim
+        end
 
-module TtcRailsBase
-  class Application < Rails::Application
-
-   config.generators do |g|
-      g.template_engine :slim
+      end
     end
-  end
-end
-
 
     config.autoload_paths << File.expand_path('../../', __FILE__)
-
-    initializer :ttc_rails_glossary do
-	     
-    end
-
   end
 end
 
